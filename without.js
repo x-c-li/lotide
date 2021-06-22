811ba3f6b9a097b260ac51f42fedf1ea8affedda
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-22 23:18:28 +0000

    added assertArraysEqual and eqArrays function

---
+++ without.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    //remember that this is an index, so the lengths are the same 
+    //for both arrays, so use the same index to refer to the numbers 
+    //in both arrays
+    if (array1[x] !== array2[x]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`😊️ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🙄️ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
