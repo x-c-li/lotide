dfb00a509863dc3b677749bbc4657bc5d1248323
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-22 23:15:17 +0000

    finished assertArraysEqual function and added messages from assertEqual to be logged when function is called

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,3 +13,12 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`😊️ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🙄️ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
