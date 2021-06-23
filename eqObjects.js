6b689d3d1ca452bfd3095b2f396cc1cdc7f9e0a2
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-23 23:19:09 +0000

    fixed up code, now all cases work. used Array.isarray and eqArrays

--- eqObjects.js
+++ eqObjects.js
@@ -6,3 +6,66 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    //remember that this is an index, so the lengths are the same 
+    //for both arrays, so use the same index to refer to the numbers 
+    //in both arrays
+    if (array1[x] !== array2[x]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+// Returns true if both objects have identical keys w identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+
+  keysObj1 = Object.keys(object1);
+  //console.log(`length of obj 1 keys: ${keysObj1.length}`);
+  keysObj2 = Object.keys(object2);
+  //console.log(`length of obj 2 keys: ${keysObj2.length}`);
+
+  if (keysObj1.length !== keysObj2.length) {
+    return false;
+  }
+
+  for (const obKey of keysObj1) {
+    //check to see if both arrays 
+    if (Array.isArray(object1[obKey]) && Array.isArray(object2[obKey])) {
+      if (!eqArrays(object1[obKey], object2[obKey])) {
+        return false;
+      }
+    } else {
+      //either both or one are not arrays 
+      if (obKey in object2) {
+        if (object1[obKey] !== object2[obKey]) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+};
+
+
+//TEST CASES
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
+
+//TEST CASES - WITH ARRAYS
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
