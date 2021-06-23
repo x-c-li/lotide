70022aa35441dacec3b424931ba271e5904627b1
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-23 21:23:10 +0000

    finished letterPositions, tested with assertArraysEqual)

---
+++ letterPositions.js
@@ -0,0 +1,61 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    //remember that this is an index, so the lengths are the same 
+    //for both arrays, so use the same index to refer to the numbers 
+    //in both arrays
+    if (array1[x] !== array2[x]) {
+      return false;
+    } 
+  }
+  return true;k
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`😊️ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🙄️ Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+
+const letterPositions = function(sentence) {
+  //return all the indices (zero-based positions) 
+  //in the string where each character is found.
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+//TEST CASE
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+
+
+//EXAMPLE OF EXPECTED OUTPUT
+// { 
+//   l: [0],
+//   i: [1, 11],
+//   g: [2],
+//   h: [3, 5, 15, 18],
+//   t: [4, 14],
+//   o: [6, 19],
+//   u: [7, 20],
+//   s: [8, 21],
+//   e: [9, 16, 22],
+//   n: [12]
+// }
+
