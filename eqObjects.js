2fc0c0668e2accf0b29b8299280479ed047d4bef
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-23 22:04:24 +0000

    added assertEqual function

---
+++ eqObjects.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊️😊️😊️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙄️🙄️🙄️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
