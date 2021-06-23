786fdad0241ef503b3e3440ede3a9d0803dc0a6c
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-23 21:35:06 +0000

    findKeyByValue function finished, didn't use Objects.key though, used a for loop with const key in object

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊️😊️😊️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙄️🙄️🙄️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//take object and value for input
+//scan object and return first key which contains given value
+//if no key w given value is found, return undefined
+const findKeyByValue = function(objectName, val) {
+  for (const key in objectName) {
+    if (objectName[key] === val) {
+      return key;
+    }
+  }
+};
+
+
+//TEST CASES
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
