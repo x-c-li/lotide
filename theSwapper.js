ead1d95f1026aba93372ae0d3bbbde5194fab236
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-28 21:18:07 +0000

    adding file

---
+++ theSwapper.js
@@ -0,0 +1,34 @@
+const swapper = function(key1, object1, key2, object2) {
+  console.log('Swap!');
+
+  object1Keys = Object.keys(object1);
+  object2Keys = Object.keys(object2);
+  // console.log(object2Keys);
+  
+  for (let k1 of object1Keys) {
+    for (let k2 of object2Keys) {
+      if (key1 === k1 && key2 === k2) {
+        replaceObj2 = object1[key1];
+        object1[key1] = object2[key2];
+        object2[key2] = replaceObj2;
+      }
+    } 
+  }
+  console.log('object1: ', object1);
+  console.log('object2: ', object2);
+};
+
+swapper('a', { a: 1 , b: 2, c: 3 }, 'c', { a: 4, b: 3, c: 5 });
+swapper('b', { a: 8 , b: 7, c: 6 }, 'd', { a: 5, b: 1, c: 2, d: 12});
+swapper('c', { a: 1 , b: 3, c: 3, d: 7 }, 'c', { a: 4, b: 0, c: 5 });
+
+//EXPECTED OUTPUT
+// Swap!
+// object1: { a: 5 , b: 2, c: 3 }
+// object2: { a: 4, b: 3, c: 1 }
+// Swap!
+// object1: { a: 8 , b: 12, c: 6 }
+// object2: { a: 5, b: 1, c: 2, d: 7}
+// Swap!
+// object1: { a: 1 , b: 3, c: 5, d: 7 }
+// object2: { a: 4, b: 0, c: 3 }
