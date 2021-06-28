97ef06e581bf977505b3d640148cf83cda5a1b97
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-28 21:41:55 +0000

    created test file and got rid of all the test code in middle

--- middle.js
+++ middle.js
@@ -1,31 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-//use assertArrayEqual to test functions like middle 
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let x = 0; x < array1.length; x++) {
-    //remember that this is an index, so the lengths are the same 
-    //for both arrays, so use the same index to refer to the numbers 
-    //in both arrays
-    if (array1[x] !== array2[x]) {
-      return false;
-    } 
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`😊️ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🙄️ Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -42,22 +14,5 @@ const middle = function(array) {
   }
 }
 
+module.exports = middle;
 
-
-//TEST CODE
-
-middle([1]) // => []
-middle([1, 2]) // => []
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
