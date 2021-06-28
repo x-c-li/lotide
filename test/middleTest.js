e17811d90723849452476406ec26204453c26c24
Xiaowei <xiaowei.c.li@gmail.com>
2021-06-28 22:19:40 +0000

    added test cases to these files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,29 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
+describe("#middle", () => {
+  it("returns [] if given [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+  it("returns [] if given [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("returns [2] if given [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("returns [3] if given [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [2, 3] if given [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3, 4] if given [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,16 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-assertEqual(tail(words).length, 2); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns empty list if list is empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-//other test cases
-const emptyList = [];
-// tail(emptyList); // no need to capture the return value since we are not checking it
-assertEqual(tail(emptyList), 0);
+  it("returns empty list if the list is 1 item long", () => {
+    assert.deepEqual(tail([1]), []);
+  });
 
-const oneList = [1];
-// console.log(tail(oneList));
-assertEqual(tail(oneList), 0);
+  it("returns 'Lighthouse' and 'Labs' if list is ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
